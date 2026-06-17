bb656961d19bd8a4b320342e5357a7fe88c5673e
Sascha Knoop <github@darkspirit510.de>
2026-06-17 21:11:06 +0200

    reader for Gitlab Advanced SAST (#186)

--- plugin/src/main/java/org/owasp/benchmarkutils/score/parsers/Reader.java
+++ plugin/src/main/java/org/owasp/benchmarkutils/score/parsers/Reader.java
@@ -82,6 +82,7 @@ public abstract class Reader {
                 new FortifyReader(),
                 new FortifySarifReader(),
                 new FusionLiteInsightReader(),
+                new GitlabAdvancedSastReader(),
                 new HCLAppScanIASTReader(),
                 new HCLAppScanSourceReader(),
                 new HCLAppScanStandardReader(),
